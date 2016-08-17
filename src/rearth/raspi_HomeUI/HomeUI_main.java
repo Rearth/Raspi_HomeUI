f69f8d447cb34f466dd0f86e09bf98bd968bf14c
Rearth <Davidus2010@gmail.com>
2016-08-17 21:53:23 +0200

    Basic Calendar/Time stuff

--- src/rearth/raspi_HomeUI/HomeUI_main.java
+++ src/rearth/raspi_HomeUI/HomeUI_main.java
@@ -13,9 +13,30 @@ import javax.swing.JFrame;
  */
 public class HomeUI_main extends javax.swing.JFrame {
 
+    String MONTHS[] ={"Nichts", "Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"};
+    String TAGE[] = {null, "Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"};
+    
+    private void firstInit() {
+        System.out.println("Hallo");
+        int date[] = rearth.raspi_HomeUI.TimeDate.TimeService.getDate();
+        int time[] = rearth.raspi_HomeUI.TimeDate.TimeService.getTime();
+        updateTime(date, time);
+    }
+    
     public HomeUI_main() {
         initComponents();
         setExtendedState(JFrame.MAXIMIZED_BOTH); 
+     }
+    
+    public void updateTime(int Date[], int Time[]) {
+        //Zeilenumbruch: "<html>Economy<br>Regularity</html>"
+        String day = Integer.toString(Date[0]);
+        String time = Integer.toString(Time[0]) + ":" + Integer.toString(Time[1]);
+        
+        int dayofWeek = rearth.raspi_HomeUI.TimeDate.TimeService.getDayOfWeek(Date);
+        
+        TimeDisplay.setText("<html><span style='font-size:55px'><font face=\"calibri\"><b></" + time + "</b></font></span><br><font size=\"5\">" + TAGE[dayofWeek] + ", " + day + "." + MONTHS[Date[1]] + "</font></html>");
+        
     }
 
     /**
@@ -29,6 +50,7 @@ public class HomeUI_main extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        TimeDisplay = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -42,6 +64,13 @@ public class HomeUI_main extends javax.swing.JFrame {
             }
         });
 
+        TimeDisplay.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        TimeDisplay.setText("Time and Date");
+        TimeDisplay.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        TimeDisplay.setMaximumSize(new java.awt.Dimension(214, 104));
+        TimeDisplay.setMinimumSize(new java.awt.Dimension(214, 104));
+        TimeDisplay.setName(""); // NOI18N
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -49,29 +78,37 @@ public class HomeUI_main extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(172, 172, 172)
+                        .addGap(129, 129, 129)
                         .addComponent(jLabel1))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(38, 38, 38)
+                        .addGap(367, 367, 367)
                         .addComponent(jButton1)))
-                .addContainerGap(176, Short.MAX_VALUE))
+                .addGap(126, 508, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(TimeDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(125, 125, 125)
+                .addGap(62, 62, 62)
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(24, 24, 24)
                 .addComponent(jButton1)
-                .addContainerGap(132, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 286, Short.MAX_VALUE)
+                .addComponent(TimeDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(22, 22, 22))
         );
+
+        TimeDisplay.getAccessibleContext().setAccessibleName("Timename");
+        TimeDisplay.getAccessibleContext().setAccessibleDescription("");
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        
         System.exit(0);
         
     }//GEN-LAST:event_jButton1ActionPerformed
@@ -101,16 +138,16 @@ public class HomeUI_main extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                HomeUI_main mainUI;
-                mainUI = new HomeUI_main();
-                mainUI.setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            HomeUI_main mainUI;
+            mainUI = new HomeUI_main();
+            mainUI.setVisible(true);
+            mainUI.firstInit();
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel TimeDisplay;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
